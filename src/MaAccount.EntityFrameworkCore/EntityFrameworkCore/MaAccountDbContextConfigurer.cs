dc5df3bcabd3bd3a8e052af5e7caac784c51febc
杨绪 <yangxuilyx@hotmail.com>
2019-03-01 21:22:20 +0800

    1.使用mysql数据库

--- src/MaAccount.EntityFrameworkCore/EntityFrameworkCore/MaAccountDbContextConfigurer.cs
+++ src/MaAccount.EntityFrameworkCore/EntityFrameworkCore/MaAccountDbContextConfigurer.cs
@@ -7,12 +7,12 @@ namespace MaAccount.EntityFrameworkCore
     {
         public static void Configure(DbContextOptionsBuilder<MaAccountDbContext> builder, string connectionString)
         {
-            builder.UseSqlServer(connectionString);
+            builder.UseMySql(connectionString);
         }
 
         public static void Configure(DbContextOptionsBuilder<MaAccountDbContext> builder, DbConnection connection)
         {
-            builder.UseSqlServer(connection);
+            builder.UseMySql(connection);
         }
     }
 }
