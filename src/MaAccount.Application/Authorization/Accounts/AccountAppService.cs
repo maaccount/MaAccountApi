ee7a7df082bab2e5d18cb9449130f135303e2a6b
杨绪 <yangxuilyx@hotmail.com>
2019-03-01 21:38:55 +0800

    1.删除不需要的接口

--- src/MaAccount.Application/Authorization/Accounts/AccountAppService.cs
+++ src/MaAccount.Application/Authorization/Accounts/AccountAppService.cs
@@ -18,23 +18,7 @@ namespace MaAccount.Authorization.Accounts
         {
             _userRegistrationManager = userRegistrationManager;
         }
-
-        public async Task<IsTenantAvailableOutput> IsTenantAvailable(IsTenantAvailableInput input)
-        {
-            var tenant = await TenantManager.FindByTenancyNameAsync(input.TenancyName);
-            if (tenant == null)
-            {
-                return new IsTenantAvailableOutput(TenantAvailabilityState.NotFound);
-            }
-
-            if (!tenant.IsActive)
-            {
-                return new IsTenantAvailableOutput(TenantAvailabilityState.InActive);
-            }
-
-            return new IsTenantAvailableOutput(TenantAvailabilityState.Available, tenant.Id);
-        }
-
+     
         public async Task<RegisterOutput> Register(RegisterInput input)
         {
             var user = await _userRegistrationManager.RegisterAsync(
--- src/MaAccount.Application/Roles/Dto/GetRolesInput.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MaAccount.Roles.Dto
-{
-    public class GetRolesInput
-    {
-        public string Permission { get; set; }
-    }
-}
